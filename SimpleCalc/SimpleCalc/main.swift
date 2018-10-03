d7b8d0d64493d7f6b4d23a84ed9287d125b049fb
rayz2007 <rayz2007@uw.edu>
2018-10-03 00:52:39 -0700

    made method

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,44 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if (args.contains("count")) {
+            return (args.count - 1)
+        } else if (args.contains("avg")) {
+            if (args.count == 1) {
+                return 0
+            }
+            var sum = 0;
+            for i in 0...(args.count - 2) {
+                let int1:Int! = Int(args[i])
+                sum += int1
+            }
+            return (sum / (args.count - 1))
+        } else if (args.contains("fact")) {
+            if (args.count == 1) {
+                return 0
+            }
+            var total = 1;
+            let num:Int! = Int(args[0])
+            for i in 1...num! {
+                total *= i
+            }
+            return total
+        } else {
+            let firstNum:Int! = Int(args[0])
+            let nextNum:Int! = Int(args[2])
+            let operate = args[1]
+            if (operate == "+") {
+                return (firstNum + nextNum)
+            } else if (operate == "-") {
+                return (firstNum - nextNum)
+            } else if (operate == "*") {
+                return (firstNum * nextNum)
+            } else if (operate == "/") {
+                return (firstNum / nextNum)
+            } else {
+                return (firstNum % nextNum)
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
